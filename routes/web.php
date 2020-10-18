10b6f7b867356eb6bfe2c0a7db36a94f1481e086
Niko <nikoventascl@gmail.com>
2020-10-17 21:12:46 -0300

    contruccion de los demas modulos

--- routes/web.php
+++ routes/web.php
@@ -16,62 +16,6 @@ Route::get('/administrador', function () {
 Route::get('/administrador', 'administradorController@index')->name('administrador');
 
 
-Route::resource('tipoEventos', 'TipoEventosController');
-Route::prefix('tipoEventos')->group(function() {
-	Route::post('/cambiarEstado', [
-        'as'   => 'tipoEventos.cambiarEstado',
-        'uses' => 'TipoEventosController@cambiarEstado',
-    ]);
-});
-
-
-Route::resource('tipoConsultas', 'TipoConsultasController');
-Route::prefix('tipoConsultas')->group(function() {
-	Route::post('/cambiarEstado', [
-        'as'   => 'tipoConsultas.cambiarEstado',
-        'uses' => 'TipoConsultasController@cambiarEstado',
-    ]);
-});
-
-Route::resource('formularioscontactos', 'FormulariosContactosController');
-Route::prefix('formularioscontactos')->group(function() {
-	Route::post('/cambiarEstado', [
-        'as'   => 'formularioscontactos.cambiarEstado',
-        'uses' => 'FormulariosContactosController@cambiarEstado',
-    ]);
-});
-
-Route::resource('viviendas', 'ViviendasController');
-Route::prefix('viviendas')->group(function() {
-    Route::post('/cambiarEstado', [
-        'as'   => 'viviendas.cambiarEstado',
-        'uses' => 'ViviendasController@cambiarEstado',
-    ]);
-});
-
-Route::get('noticias/mostrar', 'NoticiasController@mostrar')->name('noticias.mostrar');
-Route::resource('noticias', 'NoticiasController');
-Route::prefix('noticias')->group(function() {
-    Route::post('/cambiarEstado', [
-        'as'   => 'noticias.cambiarEstado',
-        'uses' => 'NoticiasController@cambiarEstado',
-    ]);
-});
-Route::resource('eventos', 'EventosController');
-Route::prefix('eventos')->group(function() {
-    Route::post('/cambiarEstado', [
-        'as'   => 'eventos.cambiarEstado',
-        'uses' => 'EventosController@cambiarEstado',
-    ]);
-});
-
-Route::resource('imagenes', 'ImagenesController');
-Route::prefix('imagenes')->group(function() {
-    Route::post('/subirImagen', [
-        'as'   => 'imagenes.subirImagen',
-        'uses' => 'ImagenesController@subirImagen',
-    ]);
-});
 
 Route::resource('usuarios', 'UserController');
 Route::prefix('usuarios')->group(function() {
